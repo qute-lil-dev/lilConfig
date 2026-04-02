8e0fca5aa71db71188d4168a2021ba6865bdaccf
qute-lil-dev <qute.lil.fox.dev@gmail.com>
2026-04-02 12:36:31 +0200

    fix text on hotkey

--- src/main/java/net/lilfox/hotkey/KeyBind.java
+++ src/main/java/net/lilfox/hotkey/KeyBind.java
@@ -178,7 +178,7 @@ public final class KeyBind {
         if (keys.isEmpty()) return "NONE";
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < keys.size(); i++) {
-            if (i > 0) sb.append('+');
+            if (i > 0) sb.append(" + ");
             sb.append(keyDisplayName(keys.get(i)));
         }
         return sb.toString();
